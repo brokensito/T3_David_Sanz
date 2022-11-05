366e88215126a631c039e92a1da94d23f2c863d5
David Sanz <dsanzfuertes.15@gmail.com>
2022-11-05 19:16:33 +0100

    comit

--- ej1.py
+++ ej1.py
@@ -1,19 +1,19 @@
 # Creamos la "Torre" con las 3 columnas.
 def TorreHanoi(discos, primera, segunda, tercera):
-    
+
     if discos==1:
-        print(movimiento(discos, primera, tercera))
+        movimiento(discos, primera, tercera)
 
     else:
         TorreHanoi(discos-1, primera, tercera, segunda)
-        print(movimiento(discos, primera, tercera))
+        movimiento(discos, primera, tercera)
         TorreHanoi(discos-1, segunda, primera, tercera)
 
     return
 
 def movimiento(discos, primera, tercera):
 
-    return "Movemos el disco {} de la torre {} a la torre {}.".format(discos,primera,tercera)
+    return print("Movemos el disco {} de la torre {} a la torre {}.".format(discos,primera,tercera))
     
 # Para cualquier numero de discos (n), primero moveremos los discos (n-1) de la torre1  a la torre2.
 # Despues, movemos el disco (n) desde la torre1 a la torre3.
---
+++ ej2.py
@@ -0,0 +1,16 @@
+def determinante(matriz):
+    
+    return
+
+
+
+
+
+if __name__=="__main__":
+    matriz = [
+        [8,5,3],
+        [5,4,3],
+        [6,2,1]
+
+    ]
+    determinante(matriz)
