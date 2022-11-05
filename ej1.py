eed06843ba8776b7824b21feb1642d8714003e06
David Sanz <dsanzfuertes.15@gmail.com>
2022-11-05 18:48:23 +0100

    terminar ej

--- ej1.py
+++ ej1.py
@@ -1,1 +1,19 @@
-def TorreHanoi(discos)
+
+def TorreHanoi(discos, primera, segunda, tercera):
+
+    if discos==1:
+        print("Movemos el disco 1 de la torre {} a la torre {}.".format(primera,tercera))
+
+    else:
+        TorreHanoi(discos-1, primera, tercera, segunda)
+        print("Movemos el disco {} de la torre {} a la torre {}.".format(discos,primera,tercera))
+        TorreHanoi(discos-1, segunda, primera, tercera)
+
+    return
+
+
+if __name__=="__main__":
+    
+    TorreHanoi(74,"1","2","3")
+
+
